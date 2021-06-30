b79d1200a140b9eb6bb95154a560ecc58e867e61
Simplício Baiano <simpliciobaiano42@gmail.com>
2021-06-29 22:18:18 -0300

    menu

--- webreact/src/App.js
+++ webreact/src/App.js
@@ -1,10 +1,12 @@
 // components
+import Header from './components/Header'
 import Footer from './components/Footer'
 import Mensagem from './components/Mensagem'
 
 function App() {
   return (
     <>
+      <Header />
       <Mensagem type="danger" />
       <Footer />
     </>
