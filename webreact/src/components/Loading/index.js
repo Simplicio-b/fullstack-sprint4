1a0c90a4c35face260e9ea3f13a061f4cddfe6aa
Simplício Baiano <simpliciobaiano42@gmail.com>
2021-06-30 15:34:34 -0300

    loading finalizado

--- webreact/src/components/Loading/index.js
+++ webreact/src/components/Loading/index.js
@@ -12,7 +12,14 @@ function Loading() {
 
     return (
         <div className={`loading ${loading.show ? "" : "loading--hide"}`}>
-            <h1>Loading ...</h1>
+            <div class="bar bar1"></div>
+            <div class="bar bar2"></div>
+            <div class="bar bar3"></div>
+            <div class="bar bar4"></div>
+            <div class="bar bar5"></div>
+            <div class="bar bar6"></div>
+            <div class="bar bar7"></div>
+            <div class="bar bar8"></div>
         </div>
     )
 }
